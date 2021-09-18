5e514b0dd0c47f557650a74943eb540d3ce32b0d
anshkamboj2005 <79920545+anshkamboj2005@users.noreply.github.com>
2021-09-18 11:29:30 +0530

    Add files via upload

---
+++ System.js
@@ -0,0 +1,14 @@
+class System{
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        FileList("black");
+        text(code,300,300);
+        if(actualCode === enteredCode.toUpperCase()){
+            return true
+        }else{
+            return false
+        }
+    }
+}
